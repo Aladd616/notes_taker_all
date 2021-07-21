cf00a72c23abbac85a9cda29c7478e42682d76e8
Boomb0x <Ghost2130@gmail.com>
2021-07-21 18:27:47 -0400

    fixed css/js problem

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 
 require('./routing/api_routes')(app);
 require('./routing/html_routes')(app);
